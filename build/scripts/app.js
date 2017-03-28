70d0290e41f27b9837b31abf20e416db2e92c7b3
Alexey <teos.nl@gmail.com>
2017-03-28 18:01:15 +0300

    98%

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -1,2 +1,97 @@
-$(function () {});
+$(function () {
+
+    webshim.setOptions('forms', {
+        //set lazyCustomMessages to true
+        lazyCustomMessages: true,
+        //show custom styleable validation bubble
+        replaceValidationUI: true
+    });
+
+    //start polyfilling
+    webshim.polyfill('forms');
+
+    // form handler
+    var body = $("body");
+    var name;
+
+    $("input[name=phone]").inputmask({
+        "mask": "+9(999)999-9999",
+        greedy: false
+    });
+
+    body.on("click", ".js-small-btn", function (e) {
+        e.preventDefault();
+
+        if (!$(".thanks").length) {
+            body.addClass("form-open");
+            $(".form-wrap_small").addClass("form-wrap_open");
+        }
+    });
+
+    $("#smallForm, #bottomForm").submit(function (e) {
+        e.preventDefault();
+        $(".form-wrap_open").removeClass("form-wrap_open");
+
+        var self = $(this);
+        var selfName = self.find("input[name=name]");
+        var selfPhone = self.find("input[name=phone]");
+        var selfEmail = self.find("input[name=email]");
+        var formData = self.serialize();
+        console.log(formData);
+
+        $("[name=name1]").val(selfName.val());
+        $("[name=phone1]").val(selfPhone.val());
+        $("[name=email1]").val(selfEmail.val());
+
+        $.ajax({
+            type: "POST",
+            url: "php/send.php",
+            data: formData,
+            success: function (data) {
+                //location = "thanks.html";
+            }
+        });
+
+        body.addClass("form-open");
+        $(".form-wrap_big").addClass("form-wrap_open");
+
+        name = selfName.val();
+
+        if (name) {
+            localStorage.setItem("centoflawname", name + ", ");
+        } else {
+            localStorage.setItem("centoflawname", "");
+        }
+    });
+
+    body.on("click", function (e) {
+        var self = $(e.target);
+
+        if (self.hasClass("form-wrap") || self.hasClass("form__close")) {
+            body.removeClass("form-open");
+            $(".form-wrap").removeClass("form-wrap_open");
+        }
+    });
+
+    $("#bigForm").submit(function (e) {
+        e.preventDefault();
+
+        var self = $(this);
+        var formData = self.serialize();
+
+        $.ajax({
+            type: "POST",
+            url: "php/sendpresent.php",
+            data: formData,
+            success: function (data) {
+                location = "thanks.html";
+            }
+        });
+    });
+
+    if ($("#thanksName").length) {
+        $("#thanksName").text(localStorage.getItem("centoflawname"));
+    };
+    // form handler
+});
 //# sourceMappingURL=app.js.map
